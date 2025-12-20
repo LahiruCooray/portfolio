d8a31381f9021785e66befd49943d60c54dfb580
LahiruCooray <lahirukanishka2000@gmail.com>
2025-12-20 23:28:54 +0530

    bug fixed

--- components/UpdateCard.tsx
+++ components/UpdateCard.tsx
@@ -63,7 +63,7 @@ export default function UpdateCard({ update }: UpdateCardProps) {
                                     <iframe
                                         width="100%"
                                         height="100%"
-                                        src={update.videoUrl.replace("watch?v=", "embed/")}
+                                        src={update.videoUrl.replace("watch?v=", "embed/").replace("youtu.be/", "youtube.com/embed/")}
                                         title="Update Video"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                         allowFullScreen
